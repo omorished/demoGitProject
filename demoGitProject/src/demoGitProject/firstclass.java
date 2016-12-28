566de09c574ca2790e72c50759194d561affc4a6
aalotaib <asta828@gmail.com>
2016-12-28 11:17:05 +0300

    update add function
    completed the add function

--- demoGitProject/src/demoGitProject/firstclass.java
+++ demoGitProject/src/demoGitProject/firstclass.java
@@ -5,6 +5,8 @@ public class firstclass {
 	public static void main(String[] args) {
 		System.out.println("Adding two numbers");
 		int x=1;
+		int y=2;
+		System.out.println("Result is: "+ x+y);
 
 	}
 
