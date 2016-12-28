26f967734d4f0914a9b8a0eef65a90a6acac5013
Ali-CCIS <Ali-CCIS@10.131.219.210>
2016-12-28 16:55:23 +0300

    added swe 211

--- demoGitProject/src/demoGitProject/firstclass.java
+++ demoGitProject/src/demoGitProject/firstclass.java
@@ -5,6 +5,7 @@ public class firstclass {
 	public static void main(String[] args) {
 		int x=5;
 		int y=7;
+		int swe211=100;
 		int z=8;
 		System.out.println("HELLLLLLLLLO");
 		System.out.println("please add two numbers");
