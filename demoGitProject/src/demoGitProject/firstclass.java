e52e3782768aca769d7500b0d193ddbf409127ad
Ali-CCIS <Ali-CCIS@10.131.219.210>
2016-12-28 11:28:34 +0300

    refactoring code

--- demoGitProject/src/demoGitProject/firstclass.java
+++ demoGitProject/src/demoGitProject/firstclass.java
@@ -3,11 +3,16 @@ package demoGitProject;
 public class firstclass {
 
 	public static void main(String[] args) {
+		int x=5;
+		int y=7;
 		System.out.println("Adding two numbers");
-		int x=1;
-		int y=2;
-		System.out.println("Result is: "+ x+y);
+		System.out.println("Result is: "+ add(x,y));
+		//add(x,y);
 
+	}
+	
+	public static int add(int x, int y){
+		return x+y;
 	}
 
 }
