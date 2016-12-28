2d242bcb0b367521062e5921606ff15288863eef
Ali-CCIS <Ali-CCIS@10.131.219.210>
2016-12-28 11:14:52 +0300

    I added first number

--- demoGitProject/src/demoGitProject/firstclass.java
+++ demoGitProject/src/demoGitProject/firstclass.java
@@ -3,7 +3,8 @@ package demoGitProject;
 public class firstclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Adding two numbers");
+		int x=1;
 
 	}
 
